e403782d1ea537dcfdedf6c40ebd5b533ab04c30
3moorebbb-sketch <3moorebbb@gmail.com>
2026-08-16 02:52:07 -0500

    Create pypi_setup_script.py for MEP Suite package
    This script sets up the MEP Suite package for distribution on PyPI, including metadata and dependencies.

---
+++ software/pypi_setup_script.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mep-suite',
+    version='3.0.0',
+    packages=find_packages(),
+    description='Mandelbrot-Euler-Planck (MEP) Architecture Software Suite. Physics-based, continuous-wave solvers and optimizers.',
+    long_description=open('README.md').read() if open('README.md').read() else '',
+    long_description_content_type='text/markdown',
+    author='3MOORE. BBB',
+    url='https://github.com/yourusername/mep-architecture', # Remember to update this URL!
+    install_requires=[
+        'torch>=2.0.0',
+        'numpy>=1.24.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    keywords='machine learning, thermodynamics, physics, optimization, kuramoto, langevin',
+)
